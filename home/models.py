66da0139d44cea650c53cf8f5dd8862500d7d3d5
AshutoshGupta <rockashu.30.ar@gmail.com>
2022-08-03 00:19:28 +0530

    after migration

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
+from pyexpat import model
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=122)
+    email = models.CharField(max_length=122)
+    phone = models.CharField(max_length=122)
+    desc = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        str = 'Name = ' + self.name + ' / Email = '+self.email
+        return str
--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
+from unicodedata import name
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 
@@ -17,6 +20,15 @@ def about(request):
 
 def contact(request):
     # return HttpResponse('this is Contact page')
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone,
+                          desc=desc, date=datetime.today())
+        contact.save()
+
     return render(request, 'contacts.html')
 
 
